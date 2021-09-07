99bfba16d8fb2226d9eb4544debf09a83ee9c3d9
Zhijing Yao <yao192@purdue.edu>
2021-09-07 14:23:18 -0400

    lab3

--- source/memory_control.sv
+++ source/memory_control.sv
@@ -12,7 +12,9 @@
 // memory types
 `include "cpu_types_pkg.vh"
 
-module memory_control (
+module memory_control 
+import cpu_types_pkg::*;
+(
   input CLK, nRST,
   cache_control_if.cc ccif
 );
@@ -20,6 +22,38 @@ module memory_control (
   import cpu_types_pkg::*;
 
   // number of cpus for cc
-  parameter CPUS = 2;
+  parameter CPUS = 1;
+
+  always_comb begin
+    ccif.iwait = 0;
+    ccif.dwait = 0;
+    ccif.iload = '0;
+    ccif.dload = '0;
+    ccif.ramstore = '0;
+    ccif.ramaddr = '0;
+    ccif.ramWEN = 0;
+    ccif.ramREN = 0;
+    ccif.ccwait = 0;
+    ccif.ccinv = 0;
+    ccif.ccsnoopaddr = '0;
+    if (ccif.dREN) begin
+      ccif.ramREN = ccif.dREN;
+      ccif.ramaddr = ccif.daddr;
+      ccif.dwait = (ccif.ramstate == BUSY || ccif.ramstate == ERROR);
+      ccif.dload = ccif.ramload;
+    end
+    else if(ccif.dWEN) begin
+      ccif.ramWEN = ccif.dWEN;
+      ccif.ramaddr = ccif.daddr;
+      ccif.dwait = (ccif.ramstate == BUSY || ccif.ramstate == ERROR);
+      ccif.ramstore = ccif.dstore;
+    end
+    else if (ccif.iREN) begin
+      ccif.ramREN = ccif.iREN;
+      ccif.ramaddr = ccif.iaddr;
+      ccif.iwait = (ccif.ramstate == BUSY || ccif.ramstate == ERROR);
+      ccif.iload = ccif.ramload;
+    end
+  end
 
 endmodule
