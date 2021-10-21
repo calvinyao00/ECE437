03f72fcfaa3d1addbd4193b52a21b80f5f357bdb
Zhiyuan Yao <yao200@purdue.edu>
2021-10-21 15:53:00 -0400

    icache working

--- source/icache.sv
+++ source/icache.sv
@@ -1,23 +1,26 @@
 // interfaces
+`include "system_if.vh"
 `include "datapath_cache_if.vh"
 `include "caches_if.vh"
 
 // cpu types
 `include "cpu_types_pkg.vh"
 
-module ICACHE(
+module icache(
   input logic CLK, nRST,
   datapath_cache_if.cache dcif,
   caches_if.icache cif
 );
+    // import types
+    import cpu_types_pkg::*;
     logic ihit, cache_hit;
     icachef_t addr;
-    icache_frame icaches[15:0];
-    icache_frame next_icaches[15:0];
+    icache_frame [15:0]icaches;
+    icache_frame [15:0]next_icaches;
     
     assign addr = icachef_t'(dcif.imemaddr);
     //assign 
-    always_ff @ (negedge CLK, negedge nRST) begin
+    always_ff @ (posedge CLK, negedge nRST) begin
         if(!nRST) begin
             icaches <= '0;
         end
@@ -26,33 +29,35 @@ module ICACHE(
         end 
     end
     assign ihit = (cif.iREN) ? ~cif.iwait : 0;
-    //assign cache_hit = icaches[addr.index].tag == addr.tag && icaches[addr.index].valid;
+    //assign cache_hit = icaches[addr.idx].tag == addr.tag && icaches[addr.idx].valid;
     always_comb begin
         cache_hit = 0;
         next_icaches = icaches;
         dcif.ihit = 0;
         cif.iREN = 0;
         cif.iaddr = 0;
+        dcif.imemload= 0;
         if (ihit) begin    
-            next_icaches[addr.index].data <= cif.iload;
-            next_icaches[addr.index].valid <= 1;
-            next_icaches[addr.index].tag <= addr.tag;
+            next_icaches[addr.idx].data <= cif.iload;
+            next_icaches[addr.idx].valid <= 1;
+            next_icaches[addr.idx].tag <= addr.tag;
         end
+
         if(dcif.halt) begin
             //halted
-		    dcif.ihit = 0;
 		    dcif.imemload = 0;
         end
         else if(dcif.imemREN && !dcif.dmemREN && !dcif.dmemWEN) begin
-            dcif.ihit = ihit | cache_hit : 0;
+            //dcif.ihit = (ihit || cache_hit);
             //Hit
-            if (icaches[addr.index].tag == addr.tag && icaches[addr.index].valid) begin
-                cache_hit = 1;
-                dcif.imemload = icaches[addr.index].data;
+            if (icaches[addr.idx].tag == addr.tag && icaches[addr.idx].valid) begin
+                dcif.ihit = 1;
+                dcif.imemload = icaches[addr.idx].data;
             end
             //miss
             else begin
-                dcif.imemload = cif_imemload;
+                dcif.ihit = ihit;
+                dcif.imemload = cif.iload;
                 cif.iREN = 1;
                 cif.iaddr = dcif.imemaddr;
             end
